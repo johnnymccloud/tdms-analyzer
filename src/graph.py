Fixed scaling of Multigraph Added INITIAL_FRAMES_NUMBER = 376 parameter

src/graph.py
@@ -1,3 +1,4 @@
+import numpy as np
 from matplotlib import style
 from matplotlib import pyplot as plt
 #from matplotlib import use as mpl_use
@@ -57,7 +58,10 @@ class GraphPanel(FloatLayout):
     def updateGraph(self, frameNumber = None, data = None):
         self.graph.updateGraph(data, frameNumber)
         if data != None:
-            self.sliderRangeUpdate(max(data))
+            if type(self.graph) is SingleGraph:
+                self.sliderRangeUpdate(max(data))
+            else:
+                self.sliderRangeUpdate(max(max(data), self.graph.indicatorLength))
 
 class SingleGraph(BoxLayout):
     def __init__(self, data, **kwargs):
@@ -153,7 +157,7 @@ class MultiGraph(BoxLayout):
     
     def clearGraph(self):
         self.ax.clear()
-        self.data = [0] * 376
+        self.data = np.zeros(len(self.data))
         self.indicatorLength = 1
         self.renderGraph()
         self.renderFrameIndicator()

src/heatmap.py
@@ -12,11 +12,11 @@ from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 class Heatmap(BoxLayout):
     
-    def __init__(self, on_hit, **kwargs):
+    def __init__(self, initial_frames_number, on_hit, **kwargs):
         super().__init__(**kwargs)
         self.hitX = 0
         self.hitY = 0
-        self.data = np.zeros((376, 256, 128))
+        self.data = np.zeros((initial_frames_number, 256, 128))
         self.dataIndex = 0
         self.fig, self.ax = plt.subplots()
         self.renderHeatmap()